265855721e5ef4bcbbe2e0262779db341e054deb
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-07-01 17:02:55 +0500

    Initial

--- CurrentLocation/SceneDelegate.swift
+++ CurrentLocation/SceneDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
@@ -17,6 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -27,8 +29,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
--- CurrentLocation/ViewController.swift
+++ CurrentLocation/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+   
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            print("Location enabled")
+            locationManager.startUpdatingLocation()
+        } else {
+            print("Location not enabled")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocations = locations[0] as CLLocation
+        
+        latitudeLabel.text = String(userLocations.coordinate.latitude)
+        longitudeLabel.text = String(userLocations.coordinate.longitude)
+        
+        let geoCoder = CLGeocoder()
+        geoCoder.reverseGeocodeLocation(userLocations) { (placemark, error ) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            if let placemark = placemark, placemark.count > 0 {
+                let locality = placemark[0].locality
+                let administativeArea = placemark[0].administrativeArea
+                let country = placemark[0].country
+                self.addressLabel.text = "Address: \(String(describing: locality)), \(String(describing: administativeArea)), \(String(describing: country))"
+            }
+        }
+    }
 }
 
